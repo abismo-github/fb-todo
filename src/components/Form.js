b75b84291cd7176d998a3c3545692ddc5de9e527
dev.abismo <dev.abismo@gamil.com>
2023-06-14 11:39:12 +0900

    feat: Tailwind 적용

--- src/components/Form.js
+++ src/components/Form.js
@@ -30,20 +30,25 @@ const Form = ({ todoData, setTodoData }) => {
     setValue("");
   };
   return (
-    
     <div>
-        <form style={{ display: "flex" }} onSubmit={handleSubmit}>
-              <input
-                type="text"
-                name="value"
-                style={{
-                flex: "10",
-                padding: "5px"}}
-                placeholder= "할일을 입력해주세요"
-                value={value}
-                onChange={handleChange}
-              />
-              <input type="submit" style={{ flex: "1"}} value="입력" />
+      <form
+        className="flex pt-2"
+        style={{ display: "flex" }}
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          name="value"
+          style={{
+          flex: "10",
+          padding: "5px"}}
+          placeholder= "할일을 입력해주세요"
+          value={value}
+          onChange={handleChange}
+          className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow"
+          />
+          <input type="submit" style={{ flex: "1" }} value="입력" className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-400"
+          />
             </form>
           </div>
   )
